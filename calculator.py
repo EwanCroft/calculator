da016ffcac3999753378eda87be5f533729d1b24
Ewan Croft <121812160+EwanCroft@users.noreply.github.com>
2023-02-08 12:58:26 +0000

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -1,26 +1,52 @@
-num_1 = float(input("What is the first number?: "))
-num_2 = float(input("What is the second number?: "))
+def is_number(num):
+    return num.replace('.', '', 1).isdigit()
 
-numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, ".", ","]
+def perform_operation(num_1, num_2, operation):
+    if operation == 1:
+        return num_1 + num_2
+    elif operation == 2:
+        return num_1 - num_2
+    elif operation == 3:
+        if num_2 == 0:
+            return "Error: Division by zero"
+        return num_1 / num_2
+    elif operation == 4:
+        return num_1 * num_2
+    else:
+        return "Invalid operation"
 
-if num_1 != numbers or num_2 != numbers:
+num_1 = input("Enter the first number:\n")
+try:
+    if not is_number(num_1):
+        raise ValueError
+    num_1 = float(num_1)
+except ValueError:
+    print("Invalid input")
+    quit()
+
+num_2 = input("Enter the second number:\n")
+try:
+    if not is_number(num_2):
+        raise ValueError
+    num_2 = float(num_2)
+except ValueError:
+    print("Invalid input")
     quit()
 
 print("1. Addition\n"
       "2. Subtraction\n"
       "3. Division\n"
       "4. Multiplication")
-funct = int(input("What is the function you would like to use?: "))
 
-if funct == 1:
-    ans = str(float(num_1 + num_2))
-    print(f"The answer is {ans}")
-elif funct == 2:
-    ans = str(float(num_1 - num_2))
-    print(f"The answer is {ans}")
-elif funct == 3:
-    ans = str(float(num_1 / num_2))
-    print(f"The answer is {ans}")
-elif funct == 4:
-    ans = str(float(num_1 * num_2))
-    print(f"The answer is {ans}")
+try:
+    operation = int(input("Enter the number corresponding to the operation you want to perform:\n"))
+except ValueError:
+    print("Invalid input")
+    quit()
+
+result = perform_operation(num_1, num_2, operation)
+
+if isinstance(result, float):
+    result = str(result).rstrip(".0")
+
+print(f"The result is:\n{result}")
